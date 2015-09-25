9c3e446d2e3d0b1d43cb0e5140d37e495c983c87
Valentin Samir <valentin.samir@crans.org>
2015-09-25 17:55:32 +0200

    lot of stuff

---
+++ procmail/forms.py
@@ -0,0 +1,283 @@
+from django import forms
+from django.forms.formsets import BaseFormSet, formset_factory
+
+from pyprocmail import procmail
+from pyprocmail.procmail import Header
+
+def set_extra(self, **kwargs):
+    self.extra = kwargs
+    return self
+
+forms.Field.set_extra = set_extra
+forms.Field.extra = {}
+
+class MetaForm(forms.Form):
+    title = forms.CharField(label='title', max_length=100)
+    comment = forms.CharField(label='comment', max_length=256, required=False)
+
+class HidableFieldsForm(object):
+    def show_init(self):
+        for field_name, field in self.fields.items():
+            if self.data and self.add_prefix(field_name) in self.data:
+                value = field.to_python(self.data[self.add_prefix(field_name)])
+            elif self.initial:
+                value = self.initial.get(field_name, field.initial)
+            else:
+                value = field.initial
+            if field.extra.get('show_if_value_not', '__RESERVED_VALUE_TYHU') == value and not self.errors.get(field_name):
+                field.show = False
+            else:
+                field.show = True
+        return ""
+
+class AssignmentBaseFormSet(BaseFormSet):
+    def clean(self):
+        variables = []
+        for form in self.forms:
+            if 'variable_name' in form.cleaned_data and not form.cleaned_data.get('DELETE', False):
+                variables.append((form.cleaned_data['variable_name'], form.cleaned_data.get('value', None)))
+        #if not variables:
+        #    raise forms.ValidationError("You need at least one assignement on a assignement satement")
+        self.variables = variables
+
+class HeaderForm(forms.Form, HidableFieldsForm):
+
+    H = forms.BooleanField(label="Flag H", help_text=Header.H.__doc__, required=False, initial=True).set_extra(show_if_value_not=True)
+    B = forms.BooleanField(label="Flag B", help_text=Header.B.__doc__, required=False, initial=False).set_extra(show_if_value_not=False)
+    h = forms.BooleanField(label="Flag h", help_text=Header.h.__doc__, required=False, initial=True).set_extra(show_if_value_not=True)
+    b = forms.BooleanField(label="Flag b", help_text=Header.b.__doc__, required=False, initial=True).set_extra(show_if_value_not=True)
+    c = forms.BooleanField(label="Flag c", help_text=Header.c.__doc__, required=False, initial=False)
+    A = forms.BooleanField(label="Flag A", help_text=Header.A.__doc__, required=False, initial=False).set_extra(show_if_value_not=False)
+    a = forms.BooleanField(label="Flag a", help_text=Header.a.__doc__, required=False, initial=False).set_extra(show_if_value_not=False)
+    E = forms.BooleanField(label="Flag E", help_text=Header.E.__doc__, required=False, initial=False).set_extra(show_if_value_not=False)
+    e = forms.BooleanField(label="Flag e", help_text=Header.e.__doc__, required=False, initial=False).set_extra(show_if_value_not=False)
+    f = forms.BooleanField(label="Flag f", help_text=Header.f.__doc__, required=False, initial=False).set_extra(show_if_value_not=False)
+    i = forms.BooleanField(label="Flag i", help_text=Header.i.__doc__, required=False, initial=False).set_extra(show_if_value_not=False)
+    r = forms.BooleanField(label="Flag r", help_text=Header.r.__doc__, required=False, initial=False).set_extra(show_if_value_not=False)
+    w = forms.BooleanField(label="Flag w", help_text=Header.w.__doc__, required=False, initial=False).set_extra(show_if_value_not=False)
+    W = forms.BooleanField(label="Flag W", help_text=Header.W.__doc__, required=False, initial=False).set_extra(show_if_value_not=False)
+    D = forms.BooleanField(label="Flag D", help_text=Header.D.__doc__, required=False, initial=False).set_extra(show_if_value_not=False)
+
+    
+
+    lockfile = forms.BooleanField(label="Use a lockfile", required=False, initial=False)
+    lockfile_path = forms.CharField(label='lockfile path', max_length=256, required=False).set_extra(show_if_value_not="")
+
+
+class ActionForm(forms.Form, HidableFieldsForm):
+
+    action_type = forms.ChoiceField(
+        label='Action type',
+        choices=[
+            (procmail.ActionSave.type, "save"),
+            (procmail.ActionForward.type, "forward"),
+            (procmail.ActionShell.type, "shell"),
+            (procmail.ActionNested.type, "nested"),
+        ]
+    )
+
+    action_param = forms.CharField(label='Parameter', max_length=256, required=False)
+
+
+    def clean(self):
+        if self.cleaned_data["action_type"] in [
+            procmail.ActionSave.type,
+            procmail.ActionForward.type,
+            procmail.ActionShell.type,
+        ] and not self.cleaned_data.get("action_param"):
+            raise forms.ValidationError("Action %s require a parameter" % self.cleaned_data["action_type"])
+
+        if self.cleaned_data["action_type"] in [procmail.ActionSave.type, procmail.ActionShell.type]:
+            self.params = (self.cleaned_data["action_param"],)
+        elif self.cleaned_data["action_type"] == procmail.ActionForward.type:
+            param = self.cleaned_data["action_param"]
+            if ',' in param:
+                param = param.split(',')
+            else:
+                param = param.split()
+            self.params = ([p.strip() for p in param],)
+        elif self.cleaned_data["action_type"] == procmail.ActionNested.type:
+            self.params = []
+
+
+
+class ConditionForm(forms.Form):
+    type = forms.TypedChoiceField(
+        label='condition type',
+        choices=[
+            ("", ""),
+            (procmail.ConditionShell.type, "shell"),
+            (procmail.ConditionSize.type, "size"),
+            (procmail.ConditionRegex.type, "regex"),
+        ],
+        required=False,
+    )
+    negate = forms.BooleanField(label="negate", required=False)
+    param = forms.CharField(label='parameter', max_length=256, required=False)
+    substitute = forms.BooleanField(label="substitute", required=False, initial=False)
+    substitute_counter = forms.IntegerField(label="substitute counter", initial=1).set_extra(show_if_value_not=1)
+    score = forms.BooleanField(label="score", required=False, initial=False).set_extra(show_if_value_not=False)
+    score_x = forms.IntegerField(label="score x", initial=1).set_extra(show_if_value_not=1)
+    score_y = forms.IntegerField(label="score y", initial=0).set_extra(show_if_value_not=0)
+    variable = forms.BooleanField(label="variable", required=False, initial=False).set_extra(show_if_value_not=False)
+    variable_name = forms.CharField(label='variable name', max_length=256, required=False, initial="").set_extra(show_if_value_not="")
+
+    def clean(self):
+        param = self.cleaned_data["param"]
+
+        if self.cleaned_data["type"] in [
+            procmail.ConditionShell.type,
+            procmail.ConditionSize.type,
+            procmail.ConditionRegex.type
+        ] and not param:
+            raise forms.ValidationError("Condition %s require a non null parameter" % self.cleaned_data["type"])
+
+        if self.cleaned_data["type"] == procmail.ConditionSize.type:
+            if (not '<' in param and not '>' in param) or ('<' in param and '>' in param):
+                raise forms.ValidationError("Condition %s parameter must be of the shape (<|>) number" % self.cleaned_data["type"])
+
+            sign = '<' if '<' in param else '>'
+            size = param.replace(self.sign, '').strip()
+            try:
+                size = int(size)
+            except ValueError:
+                raise forms.ValidationError("Condition %s parameter must be of the shape (<|>) number" % self.cleaned_data["type"])
+            self.params = (sign, size)
+        else:
+            self.params = (param, )
+
+        if self.cleaned_data["substitute"] and self.cleaned_data["substitute_counter"] < 1:
+            raise forms.ValidationError("substitute counter must be >= 1")
+
+class AssignmentForm(forms.Form):
+    variable_name = forms.CharField(label='variable name', max_length=256)
+    value = forms.CharField(label='value', max_length=256, required=False)
+
+AssignmentFormSet = formset_factory(AssignmentForm, extra=1, formset=AssignmentBaseFormSet, can_delete=True)
+ConditionFormSet = formset_factory(ConditionForm, extra=1, can_delete=True)
+
+
+class StatementForm(forms.Form):
+
+    statement = forms.ChoiceField(widget=forms.RadioSelect, choices=[
+        ('assignment', 'Assignment'),
+        ('recipe', 'Recipe'),
+
+
+    ])
+def show(self, field_name):
+    try:
+        if field_name in self._show_dict:
+            return self._show_dict[field_name]
+    except AttributeError:
+        self._show_dict = {}
+
+    i=0
+    for form in self:
+        if form.data and form.add_prefix(field_name) in form.data:
+            ini = form.fields[field_name].to_python(form.data[form.add_prefix(field_name)])
+        elif self.initial and len(self.initial)>i:
+            ini = self.initial[i].get(field_name, form.fields[field_name].initial)
+        else:
+            ini = form.fields[field_name].initial
+        if form.fields[field_name].extra.get('show_if_value_not', '__RESERVED_VALUE_TYHU') == ini and not form.errors.get(field_name):
+            self._show_dict[field_name] = False
+        else:
+            self._show_dict[field_name] = True
+            break
+        i+=1
+    return self._show_dict[field_name]
+
+def show_init(self):
+    for form in self:
+        for field_name, field in form.fields.items():
+            field.show = show(self, field_name)
+    return ""
+AssignmentFormSet.show_init = show_init
+ConditionFormSet.show_init = show_init
+
+
+
+def conditions_form_initial(conditions):
+    initials = []
+    for cond in conditions:
+        init = {'substitute':False, 'substitute_counter':1, 'negate': False, 'variable': False, 'score':False, 'score_x': 1, 'score_y': 0}
+        while cond.is_nested():
+            if cond.is_negate():
+                init['negate'] = not init['negate']
+            elif cond.is_substitute():
+                if init['substitute']:
+                    init['substitute_counter'] += 1
+                init['substitute'] = True
+            elif cond.is_variable():
+                init['variable'] = True
+                init['variable_name'] = cond.variable
+            elif cond.is_score():
+                init['score'] = True
+                init['score_x'] = cond.x
+                init['score_y'] = cond.y
+            cond = cond.condition
+        
+        init["type"] = cond.type
+        if cond.is_shell():
+            init["param"] = cond.cmd
+        elif cond.is_size():
+            init["param"] = cond.pre_render()
+        elif cond.is_regex():
+            init["param"] = cond.regex
+        elif cond.is_score():
+            init["param"] = cond.pre_render()
+
+        initials.append(init)
+    return initials
+
+
+def assignment_form_initial(assignment):
+    initials = []
+    for (variable_name, value) in assignment.variables:
+        initials.append({
+            'variable_name': variable_name,
+            'value': value
+        })
+    return initials
+
+def meta_form_initial(obj):
+    return {
+        'title': obj.meta_title,
+        'comment': obj.meta_comment,
+    }
+
+def header_form_initial(recipe):
+    lockfile = recipe.header.lockfile
+    return {
+        'H':recipe.header.H,
+        'B':recipe.header.B,
+        'h':recipe.header.h,
+        'b':recipe.header.b,
+        'c':recipe.header.c,
+        'A':recipe.header.A,
+        'a':recipe.header.a,
+        'E':recipe.header.E,
+        'e':recipe.header.e,
+        'f':recipe.header.f,
+        'i':recipe.header.i,
+        'r':recipe.header.r,
+        'w':recipe.header.w,
+        'W':recipe.header.W,
+        'D':recipe.header.D,
+
+        'lockfile': True if lockfile else False,
+        'lockfile_path': "" if isinstance(lockfile, bool) else lockfile,
+    }
+
+def action_form_initial(recipe):
+    init = {
+        'action_type': recipe.action.type,
+    }
+    if recipe.action.is_save():
+        init['action_param'] = recipe.action.path
+    if recipe.action.is_forward():
+        init['action_param'] = " ".join(recipe.action.recipients)
+    if recipe.action.is_shell():
+        init['action_param'] = recipe.action.cmd
+    return init
--- procmail/views.py
+++ procmail/views.py
@@ -1,3 +1,229 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
+from django.contrib.formtools.wizard.views import SessionWizardView
+
+import pyprocmail.procmail
+from pyprocmail import procmail
+import forms
 
 # Create your views here.
+
+class CreateStatement(SessionWizardView):
+    form_list = [
+        ("choose", forms.StatementForm),
+        ("metadata", forms.MetaForm),
+        ("assignment", forms.AssignmentFormSet),
+        ("header", forms.HeaderForm),
+        ("conditions", forms.ConditionFormSet),
+        ("action", forms.ActionForm),
+    ]
+
+    condition_dict = {
+        "assignment": lambda wizard: wizard.get_cleaned_data_for_step("choose") and wizard.get_cleaned_data_for_step("choose").get("statement") == "assignment",
+        "header": lambda wizard: wizard.get_cleaned_data_for_step("choose") and wizard.get_cleaned_data_for_step("choose").get("statement") == "recipe",
+        "conditions": lambda wizard: wizard.get_cleaned_data_for_step("choose") and wizard.get_cleaned_data_for_step("choose").get("statement") == "recipe",
+        "action": lambda wizard: wizard.get_cleaned_data_for_step("choose") and wizard.get_cleaned_data_for_step("choose").get("statement") == "recipe",
+    }
+
+    def get_template_names(self):
+        return "procmail/create.html"
+
+    def get_context_data(self, form, **kwargs):
+        context = super(CreateStatement, self).get_context_data(form=form, **kwargs)
+        form_context = []
+        for step, form in self.form_list.items():
+            if self.steps.current == step:
+                break
+            form_context.append(self.get_cleaned_data_for_step(step))
+        context.update({'form_data' : form_context})
+        return context
+
+
+    def done(self, form_list, form_dict, **kwargs):
+        typ = self.get_cleaned_data_for_step("choose")["statement"]
+        procmailrc = pyprocmail.procmail.parse("/home/valentin/.procmailrc")
+        if typ == "recipe":
+            return do_edit_recipe(self.kwargs['id'], None, procmailrc, form_dict["metadata"], form_dict["header"], form_dict["conditions"], form_dict["action"])
+        elif typ == "assignment":
+            return do_edit_assignment(self.kwargs['id'], None, procmailrc, form_dict["metadata"], form_dict["assignment"])
+
+
+def index(request):
+    procmailrc = pyprocmail.procmail.parse("/home/valentin/.procmailrc")
+    return render(request, "procmail/index.html", {"procmailrc": procmailrc})
+
+def do_edit_recipe(id, r, procmailrc, form_meta, form_header, form_condition, form_action, create=False, delete=False):
+            if r is None:
+                r = procmail.Recipe(procmail.Header(), procmail.Action())
+                r.parent = get_rule(procmailrc, id)
+                r.parent.append(r)
+                r.id = "%s.%s" % (r.parent.id, len(r.parent) - 1)
+                id = r.id
+            if form_meta.is_valid() and form_header.is_valid() and form_action.is_valid() and form_condition.is_valid():
+                # header
+                r.header.H = form_header.cleaned_data['H']
+                r.header.B = form_header.cleaned_data['B']
+                r.header.h = form_header.cleaned_data['h']
+                r.header.b = form_header.cleaned_data['b']
+                r.header.c = form_header.cleaned_data['c']
+                r.header.A = form_header.cleaned_data['A']
+                r.header.a = form_header.cleaned_data['a']
+                r.header.E = form_header.cleaned_data['E']
+                r.header.e = form_header.cleaned_data['e']
+                r.header.f = form_header.cleaned_data['f']
+                r.header.i = form_header.cleaned_data['i']
+                r.header.r = form_header.cleaned_data['r']
+                r.header.w = form_header.cleaned_data['w']
+                r.header.W = form_header.cleaned_data['W']
+                r.header.D = form_header.cleaned_data['D']
+                r.meta_title = form_meta.cleaned_data['title']
+                r.meta_comment = form_meta.cleaned_data['comment']
+                if form_header.cleaned_data['lockfile']:
+                    if form_header.cleaned_data['lockfile_path']:
+                        r.header.lockfile = form_header.cleaned_data['lockfile_path']
+                    else:
+                        r.header.lockfile = True
+                else:
+                    r.header.lockfile = False
+                # conditions
+                conditions = []
+                for fcond in form_condition:
+                    if fcond.cleaned_data.get('DELETE', False):
+                        continue
+                    if not fcond.cleaned_data.get('type'):
+                        continue
+                    cond = procmail.Condition.from_type(fcond.cleaned_data['type'])(*fcond.params)
+                    if fcond.cleaned_data['negate']:
+                        cond = procmail.ConditionNegate(cond)
+                    if fcond.cleaned_data['score']:
+                        cond = procmail.ConditionScore(fcond.cleaned_data['score_x'], fcond.cleaned_data['score_y'], cond)
+                    if fcond.cleaned_data['substitute']:
+                       for i in range(fcond.cleaned_data['substitute_counter']):
+                           cond = procmail.ConditionSubstitute(cond)
+                    if fcond.cleaned_data['variable']:
+                        cond = procmail.ConditionVariable(fcond.cleaned_data['variable_name'], cond)
+                    conditions.append(cond)
+                r.conditions = conditions
+                # action
+                if form_action.cleaned_data['action_type'] != r.action.type or form_action.cleaned_data['action_type'] != procmail.ActionNested.type:
+                    r.action = procmail.Action.from_type(form_action.cleaned_data['action_type'])(*form_action.params)
+                if create:
+                    procmailrc.write("/home/valentin/.procmailrc")
+                    return redirect("procmail:create", id=id)
+                if delete:
+                    r.parent.remove(r)
+                    procmailrc.write("/home/valentin/.procmailrc")
+                    return redirect("procmail:edit", id=".".join(id.split('.')[:-1]))
+                procmailrc.write("/home/valentin/.procmailrc")
+                return redirect("procmail:edit", id=id)
+
+def do_edit_assignment(id, r, procmailrc, form_meta, form_assignment, delete=False):
+            if r is None:
+                r = procmail.Assignment([])
+                r.parent = get_rule(procmailrc, id)
+                r.parent.append(r)
+                r.id = "%s.%s" % (r.parent.id, len(r.parent) - 1)
+                id = r.id
+            if form_meta.is_valid() and form_assignment.is_valid():
+                r.meta_title = form_meta.cleaned_data['title']
+                r.meta_comment = form_meta.cleaned_data['comment']
+                if not delete and form_assignment.variables:
+                    r.variables = form_assignment.variables
+                    procmailrc.write("/home/valentin/.procmailrc")
+                    return redirect("procmail:edit", id=id)
+                else:
+                    r.parent.remove(r)
+                    procmailrc.write("/home/valentin/.procmailrc")
+                    return redirect("procmail:edit", id=".".join(id.split('.')[:-1]))
+
+
+def get_rule(procmailrc, id):
+    ids = id.split('.')
+    r = procmailrc
+    try:
+        for i in ids:
+            r = r[int(i)]
+    except (TypeError, IndexError):
+        raise Http404()
+    return r
+
+
+def edit(request, id):
+    procmailrc = pyprocmail.procmail.parse("/home/valentin/.procmailrc")
+    r = get_rule(procmailrc, id)
+
+    params = {"procmailrc": procmailrc, "curr_stmt": r}
+    if request.method == 'POST':
+        if r.is_recipe():
+            form_meta = forms.MetaForm(request.POST, initial=forms.meta_form_initial(r), prefix="meta")
+            form_header = forms.HeaderForm(request.POST, initial=forms.header_form_initial(r), prefix="header")
+            form_action = forms.ActionForm(request.POST, initial=forms.action_form_initial(r), prefix="action")
+            form_condition = forms.ConditionFormSet(request.POST, initial=forms.conditions_form_initial(r.conditions), prefix="condition")
+            params["form_meta"] = form_meta
+            params["form_header"] = form_header
+            params["form_action"] = form_action
+            params["form_condition"] = form_condition
+            ret = do_edit_recipe(id, r, procmailrc, form_meta, form_header, form_condition, form_action, create=("action_add" in request.POST), delete=("delete_stmt" in request.POST))
+            if ret:
+                return ret
+        elif r.is_assignment():
+            form_meta = forms.MetaForm(request.POST, initial=forms.meta_form_initial(r), prefix="meta")
+            form_assignment = forms.AssignmentFormSet(request.POST, initial=forms.assignment_form_initial(r), prefix="assignment")
+            params["form_meta"] = form_meta
+            params["form_assignment"] = form_assignment
+            ret = do_edit_assignment(id, r, procmailrc, form_meta, form_assignment, delete=("delete_stmt" in request.POST))
+            if ret:
+                return ret
+    else:
+        if r.is_recipe():
+            form_meta = forms.MetaForm(initial=forms.meta_form_initial(r), prefix="meta")
+            form_header = forms.HeaderForm(initial=forms.header_form_initial(r), prefix="header")
+            form_action = forms.ActionForm(initial=forms.action_form_initial(r), prefix="action")
+            form_condition = forms.ConditionFormSet(initial=forms.conditions_form_initial(r.conditions), prefix="condition")
+            params["form_meta"] = form_meta
+            params["form_header"] = form_header
+            params["form_action"] = form_action
+            params["form_condition"] = form_condition
+        elif r.is_assignment():
+            form_meta = forms.MetaForm(initial=forms.meta_form_initial(r), prefix="meta")
+            form_assignment = forms.AssignmentFormSet(initial=forms.assignment_form_initial(r), prefix="assignment")
+            params["form_meta"] = form_meta
+            params["form_assignment"] = form_assignment
+        
+    return render(request, "procmail/edit.html", params)
+
+def up(request, id, cur_id):
+    return up_down(request, id, cur_id, lambda x,y:x-y, lambda ids, r: ids[-1] == "0")
+def down(request, id, cur_id):
+    return up_down(request, id, cur_id, lambda x,y:x+y, lambda ids, r: int(ids[-1]) == len(r) - 1)
+
+def up_down(request, id, cur_id, op, test):
+    ids = id.split('.')
+
+    procmailrc = pyprocmail.procmail.parse("/home/valentin/.procmailrc")
+    r = procmailrc
+    try:
+        for i in ids[:-1]:
+            r = r[int(i)]
+    except (TypeError, IndexError):
+        raise Http404()
+
+    if test(ids, r):
+        if not cur_id:
+            return redirect("procmail:index")
+        else:
+            return redirect("procmail:edit", id=cur_id)
+
+    i = int(ids[-1])
+    j=op(i, 1)
+    while j>0 and j<len(r) and r[j].is_comment():
+        j=op(j, 1)
+    if r[j].is_comment():
+        return redirect("procmail:edit", id=cur_id)
+    r[j], r[i] = r[i], r[j]
+    new_id = ".".join(ids[:-1] + ["%s" % j])
+    procmailrc.write("/home/valentin/.procmailrc")
+    if not cur_id:
+        return redirect("procmail:index")
+    else:
+        return redirect("procmail:edit", id=cur_id if id != cur_id else new_id)
